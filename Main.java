b857ac2296108d3a572f95f242d857d4118249e6
Cheea9337 <cheea9337@wrdsb.ca>
2020-10-06 15:41:36 +0000

    finished a5q4

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for their balance
+    System.out.println("Please enter a starting balance");
+    double balance = input.nextInt();
+
+    // ask the user for their interest rate
+    System.out.println("Please enter your interest rate");
+    double interest = input.nextInt();
+
+    // ask the user for the number of years they will invest for
+    System.out.println("Please enter the number of years you will invest for");
+    int years = input.nextInt();
+
+    double sum = 1;
+    double interestRate;
+    for(int count = 1; count <= years; count++){
+      interestRate = interest/100;
+      sum = balance * interestRate;
+      balance = balance + sum;
+    }
+    // tell the user their final balance
+    System.out.println("Your final balance would be " + "$" + balance);
     
   }
 }
